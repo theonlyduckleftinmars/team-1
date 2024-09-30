6d852d8d80b6240332450a94067ae40ca372157c
Claire258 <112729820+Claire258@users.noreply.github.com>
2024-09-29 22:22:09 -0500

    Update Main.java
    Added code comments

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import view.PlayerEntryScreen;
 import view.SplashScreen;
-
+//Dorothy Oliver code comment: sprint 2
+//Display spalsh screen and player entry screen
 public class Main {
     public static void main(String[] args) {
         SplashScreen splashScreen = new SplashScreen();
